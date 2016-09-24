fcd17ab949859523274ae323be5e38de8056b942
alainrk <alain.narko@gmail.com>
2016-09-24 07:36:19 +0200

    Sum digit nth

--- Encryption.java
+++ Encryption.java
@@ -9,8 +9,13 @@ public class Encryption {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.next();
-        int l = (int) Math.sqrt(s.length()), curr = 0;
+        int curr = 0;
         char res[] = new char[s.length()];
+
+        double sq = Math.sqrt(s.length());
+        int col = (int) sq;
+        int row = (int) Math.ceil(s.length() / col);
+        int l = (Math.floor(sq) <= row && row <= col && col <= Math.ceil(sq)) ? col : col+1;
 
         for (int i=0; i<l; i++){
           for (int j=i; j<s.length(); j+=l){
@@ -22,7 +27,7 @@ public class Encryption {
         String r = new String(res), ret = "";
 
         for (int i=0; i<s.length(); i+=l){
-          ret += r.substring(i, i+l) + " ";
+          ret += r.substring(i, Math.min(i+l, s.length())) + " ";
         }
 
         System.out.println(ret.trim());
